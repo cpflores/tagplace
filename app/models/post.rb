467c45bf9b2c22aeb9f0b60aa3638e6cd8cf2f6f
Chadwick Flores <chadwickflores@gmail.com>
2017-01-20 11:11:55 -0600

    tags update, throwing no method error

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,16 @@ class Post < ApplicationRecord
   def posted_on
     created_at.strftime 'Posted on %B %d, %Y'
   end
+
+  # This method will extract the words and find or create a Tag
+  def str_tags=(input_tags)
+    self.tags = input_tags.split(/\W+/).map do |tag|
+      Tag.find_or_create_by(name: tag.downcase)
+    end
+  end
+
+  # This method will show the existing tags in our form
+  def input_tags
+    tags.map(&:name).join(' ')
+  end
 end
